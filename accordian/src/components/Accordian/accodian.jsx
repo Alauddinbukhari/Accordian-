0e25db847978f8183c3a6de655e21d98c4f4fa39
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2024-12-02 12:25:46 -0700

    Updated css and logic

--- accordian/src/components/Accordian/accodian.jsx
+++ accordian/src/components/Accordian/accodian.jsx
@@ -1,28 +1,68 @@
-import React, { useState } from "react"
-import accordina.css"
-function Accordian(props){
+//single selection
+//multiple selection
 
-    var [itemId,setItemId]=useState(null)
+import { useState } from "react";
+import data from "./data.js";
+import "./accordina.css";
 
-    function handleClick(currentID){
-       setItemId(currentID)
+export default function Accordian() {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
 
-    }
+  function handleMultiSelection(getCurrentId) {
+    let cpyMutiple = [...multiple];
+    const findIndexOfCurrentId = cpyMutiple.indexOf(getCurrentId);
 
+    console.log(findIndexOfCurrentId);
+    if (findIndexOfCurrentId === -1) cpyMutiple.push(getCurrentId);
+    else cpyMutiple.splice(findIndexOfCurrentId, 1);
 
+    setMultiple(cpyMutiple);
+  }
 
-
-
-
-
-    return(<div class = "wrapper" onClick={ ()=>{
-        handleClick({props.id})
-    }}>
-        <h1 class="title">{props.title}</h1>
-        {itemId===props.id?<p class="content">{props.content}</p>:null}
-    </div>)
-
+  console.log(selected, multiple);
+  return (
+    <div className="acc-wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        Enable Multi Selection
+      </button>
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {enableMultiSelection
+                ? multiple.indexOf(dataItem.id) !== -1 && (
+                    <div className="acc-content ">{dataItem.answer}</div>
+                  )
+                : selected === dataItem.id && (
+                    <div className="acc-content ">{dataItem.answer}</div>
+                  )}
+              {/* {selected === dataItem.id ||
+              multiple.indexOf(dataItem.id) !== -1 ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null} */}
+            </div>
+          ))
+        ) : (
+          <div>No data found !</div>
+        )}
+      </div>
+    </div>
+  );
 }
-
-export default Accordian
